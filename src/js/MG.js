ec17d1ce970745f9a9c46011d70c3db99892f53e
Kristian Alvarez Jörgensen <krijor@kth.se>
2018-02-26 15:26:55 +0100

    A basic template for coverage

--- src/js/MG.js
+++ src/js/MG.js
@@ -1,1 +1,5 @@
 (typeof window === 'undefined' ? global : window).MG = {version: '2.11'};
+
+window.__COVERAGE__ = new Array();
+__COVERAGE__['mg_min_max_numerical'] = new Array();
+
--- tests/coverage_test.js
+++ tests/coverage_test.js
@@ -1,5 +1,6 @@
 module('data_graphic');
 
 QUnit.done( ( { total, failed, passed, runtime } ) => {
+  console.log(__COVERAGE__['mg_min_max_numerical']);
   console.log( `Total: ${total}, Failed: ${failed}, Passed: ${passed}, Runtime: ${runtime}` );
 });
