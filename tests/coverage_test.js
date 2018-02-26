b6be77a9d312730febf877d9dc2248d85ede7975
Kristian Alvarez Jörgensen <krijor@kth.se>
2018-02-26 15:04:28 +0100

    Attempting to run stuff after the tests

---
+++ tests/coverage_test.js
@@ -0,0 +1,5 @@
+module('data_graphic');
+
+QUnit.done( ( { total, failed, passed, runtime } ) => {
+  console.log( `Total: ${total}, Failed: ${failed}, Passed: ${passed}, Runtime: ${runtime}` );
+});
